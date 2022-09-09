82e9bef6ee8ba2c998b3dca3b09380eb5158e40e
DennisLCastillo <55577545+DennisLojaCastillo@users.noreply.github.com>
2022-09-09 10:05:35 +0200

    Initial commit

---
+++ src/SuperGreeter.java
@@ -0,0 +1,9 @@
+public class SuperGreeter {
+    public static void main(String[] args) {
+        SuperGreeter sg = new SuperGreeter();
+        sg.start();
+    }
+    public void start(){
+        System.out.println("Det virker!");
+    }
+}
