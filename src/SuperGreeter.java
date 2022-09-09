248c42bb5510e4b9703278572c6a95464b39cfd3
DennisLCastillo <55577545+DennisLojaCastillo@users.noreply.github.com>
2022-09-09 10:22:54 +0200

    added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? : ");
+        String name = scanner.next();
+
+
+
     }
     public void start(){
         System.out.println("Det virker!");
     }
+
+
+
 }
